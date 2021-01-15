d9376ae0ad3be468c6daedd28380397eaa634129
LabaLabaDabDab <61375062+LabaLabaDabDab@users.noreply.github.com>
2021-01-15 15:02:37 +0700

    Add files via upload

---
+++ BlackJack/score_20.cpp
@@ -0,0 +1,25 @@
+#ifndef SCORE_20_H_
+#define SCORE_20_H_
+#include "factory.h"
+#include "strategy.h"
+
+class Score_20: public Strategy{
+public:
+    Score_20(){
+        name = "score_20";
+    }
+
+    void turn(int c , int dealerFirst) override {
+        score += c;
+        if(score >= 20){
+            pass = true;
+        }
+    }
+};
+
+namespace {
+    bool b = Factory<Strategy, Strategy*(*)(), std::string>::
+    get_instance()->register_strat("Score_20", Creator<Score_20, Strategy>::create);
+}
+
+#endif
