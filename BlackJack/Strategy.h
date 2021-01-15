f9582a34f67522afcf83f8cef98b2107a992b975
LabaLabaDabDab <61375062+LabaLabaDabDab@users.noreply.github.com>
2021-01-15 14:37:21 +0700

    Delete BlackJack directory

--- BlackJack/Strategy.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Player.h"
-class Strategy
-{
-    public:
-	// Чисто виртуальный метод, который переопределяет поведение стратегии
-	virtual char GetStrategyChoice(Player player, Player opponent) { return 'x'; }
-};
